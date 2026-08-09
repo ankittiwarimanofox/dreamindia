47e32c3eb6f319bcc87f66413c1ebf4353e8f7f2
ankittiwarimanofox <ankittiwarimanofox@gmail.com>
2026-08-09 14:05:58 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,17 +35,15 @@ export default function RootLayout({
     >
       <head>
         {/* Google Tag Manager */}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N2WH6RQ8');`,
-          }}
-        />
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9X88LXL5SG"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9X88LXL5SG');
+</script>
       </head>
       <body className="min-h-full flex flex-col">
         {/* Google Tag Manager (noscript) */}
